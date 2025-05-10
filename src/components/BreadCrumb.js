c0a1dec10c78cda690cf7680c3f63fb617982f8f
Indu <wjsdlsdn1158@naver.com>
2025-05-10 16:06:12 +0900

    [feat] show path

--- src/components/BreadCrumb.js
+++ src/components/BreadCrumb.js
@@ -1,27 +1,43 @@
 import React from 'react'
-import { useLocation } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 function BreadCrumb() {
   const location = useLocation();
   const pathnames = location.pathname.split('/').filter((x) => x);
 
-  const getPathname = (segment) => {
+  const getPathname = (segment, index) => {
+    const prev = pathnames[index - 1];
+
     switch (segment) {
       case 'species':
         return 'Pokemon Species List';
       case 'pokemons':
         return 'Pokemon List';
       default:
+        if (prev === 'species') return `species-${segment}`;
+        if (prev === 'pokemons') return `pokemon-${segment}`;
         return segment;
     }
   };
 
   return (
     <nav>
-      <span>Home</span>
-      {pathnames.map((segment, i) => (
-        <span key={i}> {' > '} {getPathname(segment)}</span>
-      ))}
+      <Link to="/">Home</Link>
+      {pathnames.map((segment, i) => {
+        const to = '/' + pathnames.slice(0, i + 1).join('/');
+        const isLast = i === pathnames.length - 1;
+
+        return (
+          <span key={to}>
+            {' > '}
+            {isLast ? (
+              <span>{getPathname(segment)}</span>
+            ) : (
+              <Link to={to}>{getPathname(segment)}</Link>
+            )}
+          </span>
+        );
+      })}
     </nav>
   )
 }
